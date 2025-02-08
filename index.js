8b83d94df8cfec50329cbd64cdcb0fcc7686dbb4
Mariano-SI <marianosilva.dev@gmail.com>
2025-02-08 19:50:09 -0300

    subindo

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from "express";
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.get("/:id", (req, res) => {
     const id = req.params.id;
     console.log("Cookies: ", id);
 });
 
 app.listen(port, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port" + " " + port);
 });
