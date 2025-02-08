7b4b737c617ebc71a428efada31c706c291608ec
Mariano-SI <marianosilva.dev@gmail.com>
2025-02-08 19:44:43 -0300

    adicionando porta dinamica para azure

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from "express";
 const app = express();
-
+const port = process.env.PORT || 3000;
 app.get("/:id", (req, res) => {
     const id = req.params.id;
     console.log("Cookies: ", id);
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server is running on port 3000");
 });
